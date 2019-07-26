c921af7804065e097fa75038a803fac4fec05d65
qhy-53 <yong53jun@163.com>
2019-07-26 17:15:10 +0800

    initial commit

---
+++ src/main/java/com/haiyu/mysourcetree/MysourcetreeApplication.java
@@ -0,0 +1,13 @@
+package com.haiyu.mysourcetree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysourcetreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysourcetreeApplication.class, args);
+    }
+
+}
