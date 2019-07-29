76e8cda69af3a5b05d37ad18b6ade454caa0a50b
qhy-53 <yong53jun@163.com>
2019-07-29 08:51:44 +0800

    还原

--- src/main/java/com/haiyu/mysourcetree/controller/ControllerTest.java
+++ src/main/java/com/haiyu/mysourcetree/controller/ControllerTest.java
@@ -1,4 +1,30 @@
 package com.haiyu.mysourcetree.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Random;
+
+import static java.lang.System.currentTimeMillis;
+
+@Controller
 public class ControllerTest {
+
+    @ResponseBody
+    @RequestMapping("/random")
+    public String getRandom() {
+        Random random = new Random();
+        int firstRandom = random.nextInt(100);
+        int max = 100, min = 1;
+        int secondRandom = (int) (Math.random() * (max - min) + min);
+
+        int thirdlyRandom = (int) (Math.random() * (max - min));
+
+
+        currentTimeMillis();
+        return "" + firstRandom + "--" + secondRandom + "--" + thirdlyRandom;
+    }
+
+
 }
